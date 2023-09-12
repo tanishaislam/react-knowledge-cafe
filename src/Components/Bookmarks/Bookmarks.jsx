ad843c12ec99a9e4495d68049f42925fc156c49b
tanishaislam <dev.eva.2006@gmail.com>
2023-09-12 17:53:05 +0600

    my last commit

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -5,8 +5,8 @@ const Bookmarks = ({bookmarks, readingtime}) => {
  
     return (
         <div className="w-1/3 bg-zinc-100 pt-6">
-            <div>
-                <h3 className='text-3xl text-center'>Reading time: {readingtime}</h3>
+            <div className='mb-5'>
+                <h3 className='text-3xl text-center text-pink-700 bg-pink-100 rounded-xl p-6 mx-8 border-solid border-2 border-pink-700'>Reading time: {readingtime}</h3>
             </div>
             <p className="text-3xl font-semibold text-center pb-4">Bookmarked blogs : {bookmarks.length}</p>
             {
